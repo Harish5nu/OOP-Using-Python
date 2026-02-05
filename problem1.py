2815d7afbe94f651b16162fb09378c7149907042
Harish5nu <saudharish770@gmail.com>
2026-02-05 18:01:23 +0545

    the problem of programmer

--- problem1.py
+++ problem1.py
@@ -1,11 +1,13 @@
-#Q1. create a class "progarmming" for storing information of few programmers working at Microsoft.
+#Q1. create a class "progarmmer" for storing information of few programmers working at Microsoft.
 
-class programeer:
+class programmer:
     company="Microsoft"
     def __init__(self, name, salary, pin):
         self.name=name
         self.salary=salary
-        self.pin=pin
+        self.pin=pin 
 
-p= programeer("Harry", 120000, 2131)
+p= programmer("Harry", 120000, 2131)
 print(p.name,p.pin, p.company)
+p= programmer("Logan", 12000, 231)
+print(p.name,p.company,p.pin,p.salary)
