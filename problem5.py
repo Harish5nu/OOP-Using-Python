6e9194922a44ad3e61417fba1b37f4afd186cd5b
Harish5nu <saudharish770@gmail.com>
2026-02-08 14:42:37 +0545

    new problem

---
+++ problem5.py
@@ -0,0 +1,18 @@
+#Q. write a class Train which has methods to book a ticket, get status (no of seats) and get fare information of train running under indian Railways.
+from random import randint
+class Train:
+    def __init__(self,trainNo):
+        self.trainNo = trainNo
+
+
+    def book(self, fro, to):
+        print(f"Ticket is booked in train no : {self.trainNo} from {fro} to {to}")
+    def getStatus(self):
+        print(f"Train no : {self.trainNo} is running no time")
+    def getFare(self, fro, to):
+        print(f"Ticket fare in tain no : {self.trainNo} from {fro} to {to} is {randint(222,5555)}")
+
+t=Train(12399)
+t.book("KTM", "MNR")
+t.getStatus()
+t.book("KTM", "MNR")
